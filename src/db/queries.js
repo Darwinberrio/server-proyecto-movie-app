28bbb10a37672a764e112388c586b2cdb6949d12
Albasan08 <sandin.alba@gmail.com>
2025-12-03 12:03:20 +0100

    ADD: Admin CRUD

--- src/db/queries.js
+++ src/db/queries.js
@@ -6,21 +6,27 @@ const queries = {
 
     findUserbyID:'SELECT * FROM usuarios WHERE id_usuario=$1',
 
+    // INICIO QUERIES DE CRUD ADMIN
     peliculaExiste: "SELECT * FROM peliculas WHERE titulo=$1 AND anio=$2",
 
-    crearPelicula: "INSERT INTO peliculas(titulo, id_imagen, anio, director, genero, duracion) VALUES($1, $2, $3, $4, $5, $6) RETURNING titulo, id_imagen, anio, director, genero, duracion",
+    peliculaExisteById: "SELECT * FROM peliculas WHERE id_pelicula = $1",
 
-    actualizarPeliculaById: "UPDATE peliculas SET titulo = $1, id_imagen = $2, anio = $3, director = $4, genero = $5, duracion = $6 WHERE id_pelicula = $7 RETURNING titulo, id_imagen, anio, director, genero, duracion",
+    comprobarNombreYAnioPelicula: "SELECT titulo, anio FROM peliculas WHERE titulo = $1 AND anio = $2",
+
+    crearPelicula: "INSERT INTO peliculas(titulo, url_imagen, anio, director, genero, duracion) VALUES($1, $2, $3, $4, $5, $6) RETURNING titulo, url_imagen, anio, director, genero, duracion",
+
+    actualizarPeliculaById: "UPDATE peliculas SET titulo = $1, url_imagen = $2, anio = $3, director = $4, genero = $5, duracion = $6 WHERE id_pelicula = $7 RETURNING titulo, url_imagen, anio, director, genero, duracion",
 
     eliminarPeliculaTablaPeliculas: "DELETE FROM peliculas WHERE id_pelicula = $1",
 
     eliminarPeliculaTablaFavoritos: "DELETE FROM favoritos WHERE id_pelicula = $1",
+    // FIN QUIERIES DE CRUD ADMIN
 
     findfavoritoByFavoritoId:"SELECT favoritos.id_favorito as id_favorito,favoritos.id_usuario as id_user,peliculas.titulo as titulo,peliculas.anio as anio,peliculas.director as director,peliculas.genero as genero,peliculas.duracion as duracion FROM peliculas INNER JOIN favoritos ON peliculas.id_pelicula = favoritos.id_pelicula INNER JOIN usuarios ON favoritos.id_usuario = usuarios.id_usuario WHERE favoritos.id_usuario=$1 AND favoritos.id_pelicula=$2",
 
     findfavoritoByFavoritoId2:"SELECT favoritos.id_favorito as id_favorito,favoritos.id_usuario as id_user,peliculas.titulo as titulo,peliculas.anio as anio,peliculas.director as director,peliculas.genero as genero,peliculas.duracion as duracion FROM peliculas INNER JOIN favoritos ON peliculas.id_pelicula = favoritos.id_pelicula INNER JOIN usuarios ON favoritos.id_usuario = usuarios.id_usuario WHERE favoritos.id_usuario=$1 AND favoritos.id_favorito=$2",
 
-    findPeliculabyId:"SELECT id_pelicula as id,id_imagen as imagen,titulo anio,director,genero,duracion FROM peliculas WHERE id_pelicula=$1",
+    findPeliculabyId:"SELECT id_pelicula as id,url_imagen as imagen,titulo anio,director,genero,duracion FROM peliculas WHERE id_pelicula=$1",
 
     findUserbyID: "SELECT * FROM usuarios WHERE id_usuario=$1",
 
