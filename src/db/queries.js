79060c499cc9a001c65ffa066278eda354336961
Darwin <tu.email@example.com>
2025-12-03 12:10:33 +0100

    ADD: Actualizamos datos

--- src/db/queries.js
+++ src/db/queries.js
@@ -9,7 +9,7 @@ const queries = {
 
     peliculaExiste: "SELECT * FROM peliculas WHERE titulo=$1 AND anio=$2",
 
-    crearPelicula: "INSERT INTO peliculas(titulo, id_imagen, anio, director, genero, duracion) VALUES($1, $2, $3, $4, $5, $6) RETURNING titulo, id_imagen, anio, director, genero, duracion",
+    crearPelicula: "INSERT INTO peliculas(titulo, url_imagen, anio, director, genero, duracion) VALUES($1, $2, $3, $4, $5, $6) RETURNING titulo, id_imagen, anio, director, genero, duracion",
 
     actualizarPeliculaById: "UPDATE peliculas SET titulo = $1, id_imagen = $2, anio = $3, director = $4, genero = $5, duracion = $6 WHERE id_pelicula = $7 RETURNING titulo, id_imagen, anio, director, genero, duracion",
 
@@ -21,13 +21,13 @@ const queries = {
 
     findfavoritoByFavoritoId2:"SELECT favoritos.id_favorito as id_favorito,favoritos.id_usuario as id_user,peliculas.titulo as titulo,peliculas.anio as anio,peliculas.director as director,peliculas.genero as genero,peliculas.duracion as duracion FROM peliculas INNER JOIN favoritos ON peliculas.id_pelicula = favoritos.id_pelicula INNER JOIN usuarios ON favoritos.id_usuario = usuarios.id_usuario WHERE favoritos.id_usuario=$1 AND favoritos.id_favorito=$2",
 
-    findPeliculabyId:"SELECT id_pelicula as id,id_imagen as imagen,titulo anio,director,genero,duracion FROM peliculas WHERE id_pelicula=$1",
+    findPeliculabyId:"SELECT id_pelicula as id,url_imagen as imagen,titulo anio,director,genero,duracion FROM peliculas WHERE id_pelicula=$1",
 
     findUserbyID: "SELECT * FROM usuarios WHERE id_usuario=$1",
 
-    searchPelicula: "SELECT peliculas.id_imagen, peliculas.titulo,peliculas.anio,peliculas.director, peliculas,genero,peliculas.duracion, imagenes.originalname FROM peliculas INNER JOIN imagenes ON peliculas.id_imagen = imagenes.id_imagen WHERE peliculas.titulo=$1",
+    searchPelicula: "SELECT peliculas.url_imagen, peliculas.titulo,peliculas.anio,peliculas.director, peliculas.genero,peliculas.duracion FROM peliculas  WHERE peliculas.titulo=$1",
 
-    detalleById:"SELECT peliculas.id_imagen, peliculas.titulo,peliculas.anio,peliculas.director, peliculas,genero,peliculas.duracion, imagenes.originalname FROM peliculas INNER JOIN imagenes ON peliculas.id_imagen = imagenes.id_imagen WHERE peliculas.id_pelicula=$1",
+    detalleById:"SELECT peliculas.url_imagen, peliculas.titulo,peliculas.anio,peliculas.director, peliculas.genero,peliculas.duracion FROM peliculas WHERE peliculas.id_pelicula=$1",
 
     addFavorito: "INSERT INTO favoritos (id_usuario, id_pelicula) VALUES ($1, $2)",
 
